7a2d3d6463d9c110a3b8534deadeedfd03f22f4c
Aadit Trivedi <aadit.trivedi03@gmail.com>
2023-12-03 03:47:08 -0500

    add teammember model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
+from enum import Enum
 from django.db import models
 
 # Create your models here.
+
+
+
+class ROLE(Enum):
+    ADMIN = "admin"
+    REGULAR = "regular"
+
+    @classmethod
+    def choices(cls):
+        return [(choice.value, choice.name) for choice in cls]
+
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField(unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    role = models.CharField(max_length=10, choices=ROLE.choices(), default=ROLE.REGULAR.value)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
